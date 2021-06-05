2b9fe345f2008e75af98362c1751c9db01d1c7c6
github-kamilk <kamil.kowalski51@onet.pl>
2021-06-05 13:29:34 +0200

    Adding background

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import pygame
+import pygame, time, sys, random
 from pygame.locals import *
-import random
+from pygame import mixer
 
 pygame.init()
 
@@ -8,65 +8,139 @@ FPS = 30
 framesPerSec = pygame.time.Clock()
 
 black = (0, 0, 0)
+red = (255, 0, 0)
 
-window = pygame.display.set_mode((550, 750))
+window = pygame.display.set_mode((700, 750))
 window.fill(black)
-pygame.display.set_caption("Kart")
+pygame.display.set_caption("Racing Game")
+
+speed = 5
 
 SCREEN_WIDTH, SCREEN_HEIGHT = pygame.display.get_surface().get_size()
+
+mixer.init()
+mixer.music.load("data/crash.mp3")
+mixer.music.set_volume(0.1)
 
 
 class Obstacle(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("data/block.png")
-        self.surface = pygame.Surface((256, 40))
-        self.rect = self.surface.get_rect(center=(random.randint(100, 1000), 0))
+        self.image = pygame.image.load("data/scaledBlock.png")
+        self.surface = pygame.Surface((self.image.get_width(), self.image.get_height()))
+        self.rect = self.surface.get_rect(center=(random.randint(100, 750), random.randint(-300, 0)))
 
-    def move(self):
-        self.rect.move_ip(0,10)
+    def move(self, score):
+        self.rect.move_ip(0, speed)
         if self.rect.bottom > SCREEN_HEIGHT:
-            self.rect.center = (random.randint(100,1000),0)
+            self.rect.center = (random.randint(100, 750), random.randint(-300, 0))
+            score += 1
+        return score
 
     def draw(self, surface):
-        surface.blit(self.image,self.rect)
+        surface.blit(self.image, self.rect)
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("data/ferrari.png")
-        self.surface = pygame.Surface((115, 240))
-        self.rect = self.surface.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT-300))
+        self.image = pygame.image.load("data/scaledFerrari.png")
+        self.surface = pygame.Surface((self.image.get_width(), self.image.get_height()))
+        self.rect = self.surface.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT - 80))
 
     def draw(self, surface):
-        surface.blit(self.image,self.rect)
+        surface.blit(self.image, self.rect)
 
     def update(self):
         pressedKeys = pygame.key.get_pressed()
 
         if self.rect.left > 0:
             if pressedKeys[K_LEFT]:
-                self.rect.move_ip(-5,0)
+                self.rect.move_ip(-8, 0)
 
         if self.rect.right < SCREEN_WIDTH:
             if pressedKeys[K_RIGHT]:
-                self.rect.move_ip(5,0)
+                self.rect.move_ip(8, 0)
+
+
+class Background():
+    def __init__(self):
+        self.backgroundImage = pygame.image.load("data/scaledBackground.png")
+        self.rectangleBGimage = self.backgroundImage.get_rect()
+
+        self.bgY1 = 0
+        self.bgX1 = 0
+
+        self.bgY2 = -self.rectangleBGimage.height
+        self.bgX2 = 0
+
+        self.moveSpeed = speed
+
+    def update(self):
+        self.bgY1 += self.moveSpeed
+        self.bgY2 += self.moveSpeed
+
+        if self.bgY1 > self.rectangleBGimage.height:
+            self.bgY1 = -self.rectangleBGimage.height
+
+        if self.bgY2 > self.rectangleBGimage.height:
+            self.bgY2 = -self.rectangleBGimage.height
+
+    def render(self):
+        window.blit(self.backgroundImage, (self.bgX1, self.bgY1))
+        window.blit(self.backgroundImage, (self.bgX2, self.bgY2))
+
+
+background = Background()
+
+INCREASE_SPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(INCREASE_SPEED, 3000)
 
 P1 = Player()
 O1 = Obstacle()
+O2 = Obstacle()
+O3 = Obstacle()
+
+obstacleGroup = pygame.sprite.Group()
+obstacleGroup.add(O1)
+obstacleGroup.add(O2)
+obstacleGroup.add(O3)
+
+font = pygame.font.SysFont("Verdana", 60)
+gameOver = font.render("Game Over!", True, red)
+
+score = 0
 
 while True:
+    scoreRender = font.render("Score: " + str(score), True, red)
+    background.update()
+    background.render()
+    window.blit(scoreRender, (0, 0))
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
 
-    P1.update()
-    O1.move()
+        if event.type == INCREASE_SPEED:
+            speed += 0.5
 
-    window.fill(black)
+    if pygame.sprite.spritecollideany(P1, obstacleGroup):
+        window.fill(black)
+        window.blit(gameOver, (150, 150))
+        pygame.display.update()
+        mixer.music.play()
+        time.sleep(4)
+        pygame.quit()
+
+    for obstacle in obstacleGroup:
+        score = obstacle.move(score)
+        obstacle.draw(window)
+
+    P1.update()
 
     P1.draw(window)
     O1.draw(window)
+    O2.draw(window)
 
     pygame.display.update()
     framesPerSec.tick(FPS)
